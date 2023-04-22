99ddd379db2f115afbd36d61be91b13b0f5816ec
CharlesChinedum <charleschinedum2@gmail.com>
2023-04-22 06:08:29 +0100

    initial commit

---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { TouchableOpacity, Image } from "react-native";
+
+import styles from "./screenheader.style";
+
+const ScreenHeaderBtn = ({ iconUrl, dimension }) => {
+  const handlePress = () => {
+    console.log("pressed");
+  };
+
+  return (
+    <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+      <Image
+        source={iconUrl}
+        resizeMode="cover"
+        style={styles.btnImg(dimension)}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default ScreenHeaderBtn;
---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import useFetch from "../../../hook/useFetch";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter } from "expo-router";
+
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+
+const Popularjobs = () => {
+  const router = useRouter();
+
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React Developer",
+    num_pages: "1",
+  });
+
+  const [selectedJob, setSelectedJob] = useState();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Porpular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>View All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Some went wrong</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
+              <PopularJobCard item={item} selectedJob={selectedJob} />
+            )}
+            keyExtractor={(item) => item.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
+//50:36
